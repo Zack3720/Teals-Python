3122fa8461546e0c975b49d875adb1f20f6ca5b3
Zack Crouse <zackjcroise@gmail.com>
2020-10-01 16:28:40 -0700

    Moved all the dictionaries into one

--- Pokemon Porject/PokemonTypes.py
+++ Pokemon Porject/PokemonTypes.py
@@ -3,108 +3,124 @@ import os
 
 clear = lambda: os.system('cls')
 
-attacks = {
+pokemonDictionary = {
     'grass' : {
-        'Leaf Storm' : {
-        'power' : 130,
-        'accuracy' : 90,
+        'attacks' : {
+            'Leaf Storm' : {
+                'power' : 130,
+                'accuracy' : 90,
+            },
+            'Mega Drain' : {
+                'power' : 50,
+                'accuracy' : 100,
+            },
+            'Razor Leaf' : {
+                'power' : 55,
+                'accuracy' : 95,
+            }
         },
-        'Mega Drain' : {
-            'power' : 50,
-            'accuracy' : 100,
+        'types' : {
+            'Bulbasoar' : {
+                'HP' : 60,
+                'AP' : 40,
+            },
+            'Bellsprout' : {
+                'HP' : 40,
+                'AP' : 60,
+            }, 
+            'Oddish' : {
+                'HP' : 50,
+                'AP' : 50,
+            }
         },
-        'Razor Leaf' : {
-            'power' : 55,
-            'accuracy' : 95,
-        }
+        'weak against' : 'fire'
     },
     'fire' : {
-        'Ember' : {
-            'power' : 60,
-            'accuracy' : 100,
+        'attacks' : {
+            'Ember' : {
+                'power' : 60,
+                'accuracy' : 100,
+            },
+            'Fire Punch' : {
+                'power' : 85,
+                'accuracy' : 80,
+            },
+            'Flame Wheel' : {
+                'power': 70,
+                'accuracy' : 90,
+            }
         },
-        'Fire Punch' : {
-            'power' : 85,
-            'accuracy' : 80,
+        'types' : {
+            'Charmainder' : {
+                'HP' : 25,
+                'AP' : 70,
+            },
+            'Ninetails' : {
+                'HP' : 30,
+                'AP' : 50,
+            },
+            'Ponyta' : {
+                'HP' : 40,
+                'AP' : 60,
+            }
         },
-        'Flame Wheel' : {
-            'power': 70,
-            'accuracy' : 90,
-        }
+        'weak against' : 'water'
     },
     'water' : {
-        'Bubble' : {
-            'power' : 40,
-            'accuracy' : 100,
+        'attacks' : {
+            'Bubble' : {
+                'power' : 40,
+                'accuracy' : 100,
+            },
+            'Hydro Pump' : {
+                'power' : 185,
+                'accuracy' : 30,
+            },
+            'Surf' : {
+                'power' : 70,
+                'accuracy' : 90,
+            }
         },
-        'Hydro Pump' : {
-            'power' : 185,
-            'accuracy' : 30,
+        'types' : {
+            'Squirtle' : {
+                'HP' : 80,
+                'AP' : 20,
+            },
+            'Psyduck' : {
+                'HP' : 70,
+                'AP' : 40,
+            },
+            'Polyway' : {
+                'HP' : 50,
+                'AP' : 50,
+            }
         },
-        'Surf' : {
-            'power' : 70,
-            'accuracy' : 90,
-        }
+        'weak against' : 'grass'
     }
 }
 
-pokemonTypes = {
-    'grass' : {
-        'Bulbasoar' : {
-            'HP' : 60,
-            'AP' : 40,
-        },
-        'Bellsprout' : {
-            'HP' : 40,
-            'AP' : 60,
-        }, 
-        'Oddish' : {
-            'HP' : 50,
-            'AP' : 50,
-        }
-    },
-    'fire' : {
-        'Charmainder' : {
-            'HP' : 25,
-            'AP' : 70,
-        },
-        'Ninetails' : {
-            'HP' : 30,
-            'AP' : 50,
-        },
-        'Ponyta' : {
-            'HP' : 40,
-            'AP' : 60,
-        }
-    },
-    'water' : {
-        'Squirtle' : {
-            'HP' : 80,
-            'AP' : 20,
-        },
-        'Psyduck' : {
-            'HP' : 70,
-            'AP' : 40,
-        },
-        'Polyway' : {
-            'HP' : 50,
-            'AP' : 50,
-        }
-    }
-}
-
-class Pokemon(ABC):
+class Pokemon():
     pType = None
+    pElement = None
     maxHealth = None
     health = None
     attackPower = None
     weakAgainst = None
 
-    def __init__(this,pokemonType):
+    def __init__(this,pokemonElement,pokemonType):
+        if pokemonDictionary[pokemonElement] == None:
+            raise ValueError('Pokemon element is invalid')
+        if pokemonDictionary[pokemonElement]['type'][pokemonType] == None:
+            raise ValueError('Pokemon type is invalid') 
+        this.pElement = pokemonElement
         this.pType = pokemonType
+        maxHealth = pokemonDictionary[pokemonElement]['types'][pokemonType]['HP']
+        health = maxHealth
+        attackPower = pokemonDictionary[pokemonElement]['types'][pokemonType]['AP']
+        weakAgainst = pokemonDictionary[pokemonElement]['weak against']
         super().__init__
 
-    @abstractmethod
+    
     def attack(this, pokemon1, damage):
         pokemon1.takeDamage(damage, this.pType)
     
@@ -117,33 +133,55 @@ class Pokemon(ABC):
             this.health -= damage * 1.5
         else:
             this.health -= damage
+        
+        if this.health 
     
-    @abstractmethod
-    def setType():
-        pass
+    
+    def setType(this, pokemonType,):
+        if pokemonDictionary['water']['type'][pokemonType] == None:
+            raise ValueError('Pokemon type is invalid') 
+        this.pType = pokemonType
 
-    @abstractmethod
+    
     def setAttack():
         pass
 
-    @abstractmethod
+    
     def getAttacks():
         pass
 
 class GrassPokemon(Pokemon):
+    global grassDictionary
     
-    def __init__(this,pokemonType):
-        this.pType = pokemonType
+    def __init__():
         super().__init__
+
+    def setType(this,pokemonType):
+        if pokemonDictionary['grass']['type'][pokemonType] == None:
+            raise ValueError('Pokemon type is invalid') 
+        this.pType = pokemonType
 
 class FirePokemon(Pokemon):
+    global fireDictionary
 
-    def __init__(this,pokemonType):
-        this.pType = pokemonType
+    def __init__():
         super().__init__
+
+    def setType(this,pokemonType):
+        if pokemonDictionary['fire']['type'][pokemonType] == None:
+            raise ValueError('Pokemon type is invalid') 
+        this.pType = pokemonType
 
 class WaterPokemon(Pokemon):
+    global pokemonDictionary
 
-    def __init__(this,pokemonType):
-        this.pType = pokemonType
+    def __init__():
         super().__init__
+
+    def setType(this,pokemonType):
+        if pokemonDictionary['water']['type'][pokemonType] == None:
+            raise ValueError('Pokemon type is invalid') 
+        this.pType = pokemonType
+
+    def getType(this):
+        return this.pType
