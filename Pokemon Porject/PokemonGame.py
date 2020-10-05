8fd3005beb0f9b03363ade2cb4f6b0b5273ba2fd
Zack Crouse <zackjcroise@gmail.com>
2020-10-05 07:22:18 -0700

    Added the PokemonGame code

--- Pokemon Porject/PokemonGame.py
+++ Pokemon Porject/PokemonGame.py
@@ -1,11 +1,31 @@
 from PokemonTypes import Pokemon
+from Player import UserPlayer
+from Player import CompPlayer
 from consolemenu import *
 from consolemenu.items import *
 import os 
 clear = lambda: os.system('cls')
-
-clear()
-
 menu = SelectionMenu([''])
 
-menu.get_selection(['Ok'],title='',show_exit_option=False)
+menu.get_selection(['Play'],title='Welcome to Zack\'s Pokemon Game!',show_exit_option=False)
+
+user = UserPlayer()
+comp = CompPlayer()
+
+user.askPokemon(3)
+comp.askPokemon(3,user.getNames())
+menu.get_selection(['Fight!'],title='Your opponent has chosen their Pokemon',show_exit_option=False)
+num = 1
+while not(user.isDefeated()) and not(comp.isDefeated()):
+    if num == 1:
+        user.takeTurn(comp.getCurrentPokemon())
+        num = 0
+    else:
+        comp.takeTurn(user.getCurrentPokemon())
+        num = 1
+
+if user.isDefeated():
+    menu.get_selection(['Quit'],title='You lost! Your opponent won!',show_exit_option=False)
+else:
+    menu.get_selection(['Quit'],title='You Won! You beat your opponent!',show_exit_option=False)
+
